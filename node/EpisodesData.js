470e3fbbe387bc0ac162672e7c6d42e7c5f8d2e4
chumo <jmb.jesus@gmail.com>
2016-02-15 22:27:18 +0100

    imputed null and zero values in EpisodesData.json and group characters by their homeworlds

--- node/EpisodesData.js
+++ node/EpisodesData.js
@@ -1,4 +1,6 @@
-// Node.js script to build the data set data/EpisodesData.json from the Star Wars API with sorted and filtered information for the viz
+// Node.js script to build the data set data/EpisodesData.json
+// from the Star Wars API with sorted and filtered information for the viz.
+// The characters from every episode are groupped by their homeworlds
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var req = new XMLHttpRequest();
@@ -11,34 +13,49 @@ function getJSON(url){ // return a json from a synchronous http request
     return JSON.parse(req.responseText);
 }
 
+
 // Retrieve the data
 var data = getJSON("https://raw.githubusercontent.com/chumo/Star_Wars/gh-pages/data/StarWarsData.json");
+
 var films = us.sortBy(data.films,"id");
 
 var episodes = us.map(films,function(film){
-    var filmPlanets = us.map(film.planets,function(d){return us.findWhere(data.planets,{id:d})});
-    var filmPlanetsName = us.map(filmPlanets, function(d){return d.name;});
-    var filmPlanetsDiameter = us.map(filmPlanets, function(d){return d.diameter;});
-    var filmPlanetsPopulation = us.map(filmPlanets, function(d){return d.population;});
-    console.log(filmPlanets)
+    // var filmPlanets = us.map(film.planets,function(d){return us.findWhere(data.planets,{id:d})});
+    // var filmPlanetsName = us.map(filmPlanets, function(d){return d.name;});
+    // var filmPlanetsDiameter = us.map(filmPlanets, function(d){return d.diameter;});
+    // var filmPlanetsPopulation = us.map(filmPlanets, function(d){return d.population;});
 
-    var planets = us.map(filmPlanets, function(d,i){
+    var filmPeople = us.map(film.people,function(d){return us.findWhere(data.people,{id:d})});
+    var filmPeoplePlanet = us.map(filmPeople,function(d){return d.planet});
+    var filmPeoplePlanetObject = us.map(filmPeoplePlanet,function(d){return us.findWhere(data.planets,{id:d})});
+    var filmPeoplePlanetName = us.map(filmPeoplePlanetObject,function(d){return d.name;});
+
+    var filmUniquePlanetsName = us.unique(filmPeoplePlanetName);
+    // var filmPeoplePlanetDiameter = us.map(filmPeoplePlanetObject,function(d){return d.diameter;});
+
+    var filmUniquePlanets = us.map(filmUniquePlanetsName,function(d){return us.findWhere(data.planets,{name:d})});
+    var filmUniquePlanetsDiameter = us.map(filmUniquePlanets, function(d){return (d.diameter == null || d.diameter == 0) ? 9999 : d.diameter;}); //unknown diameters or 0 are assigned to be 9999
+    var filmUniquePlanetsPopulation = us.map(filmUniquePlanets, function(d){return (d.population == null || d.population == 0) ? 999999 : d.population;}); //unknown populations or 0 are assined to be 999999
+
+    var planets = us.map(filmUniquePlanetsName, function(d,i){
+        var peopleInPlanet = us.filter(filmPeople,function(person){return d == us.findWhere(data.planets,{id:person.planet}).name;});
+
         return {
-            name:filmPlanetsName[i],
-            population:filmPlanetsPopulation[i],
-            diameter:filmPlanetsDiameter[i]
+            name:filmUniquePlanetsName[i],
+            population:filmUniquePlanetsPopulation[i],
+            diameter:filmUniquePlanetsDiameter[i],
+            people: us.map(peopleInPlanet,function(person){
+                var thisSpecies = us.findWhere(data.species,{id:person.species});
+                return {
+                    name:person.name,
+                    species:(thisSpecies == undefined) ? "unknown" : thisSpecies.name
+                };
+                })
         };
         });
 
     return {title:film.title, opening_crawl:film.opening_crawl, planets:planets};
 });
-
-//
-// var filmPeople = us.map(film.people,function(d){return us.findWhere(data.people,{id:d})});
-// var filmPeoplePlanet = us.map(filmPeople,function(d){return d.planet});
-// var filmPeoplePlanetObject = us.map(filmPeoplePlanet,function(d){return us.findWhere(data.planets,{id:d})});
-// var filmPeoplePlanetName = us.map(filmPeoplePlanetObject,function(d){return d.name;});
-// var filmPeoplePlanetDiameter = us.map(filmPeoplePlanetObject,function(d){return d.diameter;});
 
 
 var episodesText = JSON.stringify(episodes);
